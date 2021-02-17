63f11425753a263526c9d83efa8e9d6caac84d87
aidanwolf <aidan3dbit@gmail.com>
2021-02-17 03:51:32 -0500

    supports 3d on opensea, per-client custom metadata

--- functions/handlers.js
+++ functions/handlers.js
@@ -3,8 +3,6 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 const request = require("request");
 
-request.debug = true;
-
 admin.initializeApp();
 
 var db = admin.database();
@@ -23,15 +21,46 @@ function log(msg) {
 
 function getContentType(clients, referringDomain, browser) {
 
-	let contentType = "video/mp4"; // fallback
+	let contentType = "image/gif"; // fallback
+	let fileExtension = "gif"
 
 	_.forEach(clients, (o) => {
 		if (referringDomain.includes(o.domain)) {
 			contentType = o.contentType;
+			fileExtension = o.fileType;
 		}
 	});
 
-	return contentType;
+	return [contentType, fileExtension];
+
+}
+
+function hailMary(clients, browser) {
+
+	let referrer = '';
+
+	_.forEach(clients, (o) => {
+		if (browser.includes(o.API)) {
+			log("hailMary success");
+			referrer = o.domain;
+		}
+	});
+
+	return referrer;
+
+}
+
+function getCustomMetadata(clients, referringDomain) {
+
+	let metadata = {};
+
+	_.forEach(clients, (o) => {
+		if (referringDomain.includes(o.domain) && o.metadata) {
+			metadata = o.metadata;
+		}
+	});
+
+	return metadata;
 
 }
 
@@ -41,109 +70,34 @@ module.exports.renderIndex = (req, res) => {
 
 module.exports.handlePaulieMagically = (req, res) => {
 
-	log("version 0");
-
-	const referringDomain = req.get('Referrer') || '';
-
-	if(!referringDomain) {
-		return res.render('gallery', { title: "ipfs.kevaid.com" });
-	}
-
 	db.ref().once("value").then(snap => {
+		const data = snap.val();
+		const referringDomain = req.get('Referrer') || hailMary(data.clients, req.useragent.browser);
 
-		const data = snap.val()
-		const contentType = getContentType(data.clients, referringDomain)
-		const fileExtension = contentType.split("/")[1];
+		log("browser = " + req.useragent.browser)
+		log("referrer = " +  referringDomain);
+		log("origin = " + req.get('origin'));
+
+		if(!referringDomain) {
+				return res.render('gallery', { title: "ipfs.kevaid.com" });
+		}
+
+		const fileInfo = getContentType(data.clients, referringDomain);
+		const contentType = fileInfo[0];
+		const fileExtension = fileInfo[1];
+
 		const assetHash = data.assets.paulie[fileExtension];
-		const assetUrl = `https://ipfs.fleek.co/ipfs/${assetHash}`;
+		//const assetUrl = `https://ipfs.fleek.co/ipfs/${assetHash}?filename=file.${fileExtension}`;
+		const assetUrl = `https://aidanwolf-team-bucket.storage.fleek.co/PAULIE_NFT.${fileExtension}`;
 
-		log(req.useragent.browser + " on " + referringDomain + " requesting file  " + contentType)
+		log(req.useragent.browser + " on " + referringDomain + " requesting file  " + contentType);
+		log("returning data at " + assetUrl);
 
 		res.set('Cache-Control', 'no-store');
 		res.set('X-DNS-Prefetch-Control', 'off');
 		res.set('Access-Control-Allow-Origin', '*');
 		res.set('Content-Type', contentType);
 
-		log("mp4 check " + fileExtension);
-		if (fileExtension == "mp4") {
-
-			const range = req.get("Range");
-
-			log("range = " + range);
-
-			if (range) {
-
-				const options = {
-					url: assetUrl,
-					headers: {
-						'Range': range
-					}
-				};
-
-				return request.get(options).on('response', function(response) {
-
-					log(response.statusCode) // 200
-    			log(response.headers['content-type']) // 'image/png'
-
-					/** Extracting Start and End value from Range Header */
-				 let [start, end] = range.replace(/bytes=/, "").split("-");
-				 start = parseInt(start, 10);
-				 end = end ? parseInt(end, 10) : size - 1;
-
-				 if (!isNaN(start) && isNaN(end)) {
-					 start = start;
-					 end = size - 1;
-				 }
-				 if (isNaN(start) && !isNaN(end)) {
-					 start = size - end;
-					 end = size - 1;
-				 }
-
-				 // Handle unavailable range request
-				 if (start >= size || end >= size) {
-					 // Return the 416 Range Not Satisfiable.
-					 res.writeHead(416, {
-						 "Content-Range": `bytes */${size}`
-					 });
-					 return res.end();
-				 }
-
-				 /** Sending Partial Content With HTTP Code 206 */
-				 res.writeHead(206, {
-					 "Content-Range": `bytes ${start}-${end}/${size}`,
-					 "Accept-Ranges": "bytes",
-					 "Content-Length": end - start + 1,
-					 "Content-Type": "video/mp4"
-				 });
-
-			 }).pipe(res);
-
-			} else {
-				return request.get(assetUrl).pipe(res => res.status(206).send());
-			}
-
-
-
-			//
-			// const videoSize = response.headers['content-length'];
-			// // Parse range
-			// const start = 0
-			// const end = videoSize - 1
-			//
-			// request.get(assetUrl).on('response', function(response) {
-    	// 		log(response.statusCode) // 200
-			// 		log(response.headers);
-			//
-			// 				res.set('Content-Type', response.headers['content-type']);
-			// 				res.set('Content-Range', `bytes ${start}-${end}/${videoSize}`);
-			// 				res.set('Content-Length', response.headers['content-length']);
-			// 				res.set('Accept-Ranges', 'bytes');
-			//
-  		// 	}).pipe(res);
-
-
-
-		}
 		return request.get(assetUrl).pipe(res);
 	});
 }
@@ -165,15 +119,22 @@ module.exports.handlAssetByKind = async (req, res) => {
 module.exports.handleMetaData = async (req, res) => {
 
 	const data = await db.ref().once("value").then(snap => snap.val());
-	const referringDomain = req.get('Referrer') || '';
-	const contentType = getContentType(data.clients, referringDomain)
+	const referringDomain = req.get('Referrer') || hailMary(data.clients, req.useragent.browser);
+	const fileInfo = getContentType(data.clients, referringDomain);
+	const contentType = fileInfo[0];
+
+	log("browser = " + req.useragent.browser)
+	log("referrer = " +  referringDomain);
+	log("origin = " + req.get('origin'));
 
 	const metadata = {
 		...data.metadata[req.params.hash],
+		...getCustomMetadata(data.clients, referringDomain),
 		mimeType: contentType
 	}
 
-	log(referringDomain +  " from MimeType: " + contentType);
+	log("metadata request: " + referringDomain +  " from MimeType: " + contentType);
+	log("returning: " + JSON.stringify(metadata));
 
 	res.setHeader('Content-Type', 'application/json');
 	res.end(JSON.stringify(metadata));
